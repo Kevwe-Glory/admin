e4ef4b43d04cce3c3f2cfb278c917e7ac456ffa9
Glory Ajishebiyawo <ajishebiyawoglory@gmail.com>
2026-02-13 15:03:57 +0100

    Updated login logic

--- app/api/admin/login/route.ts
+++ app/api/admin/login/route.ts
@@ -1,39 +1,44 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   try {
-  const { email, password } = await req.json()
+    const { email, password } = await req.json();
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}auth/sign-in`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password }),
-  })
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}auth/sign-in`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      },
+    );
+    console.log('API BASE URL:', process.env.NEXT_PUBLIC_API_BASE_URL);
 
-  const json = await res.json()
+    const json = await res.json();
 
-  if (!json.status || !json.data?.token) {
+    if (!json.status || !json.data?.token) {
+      return NextResponse.json(
+        { message: json.message || 'Invalid credentials' },
+        { status: 401 },
+      );
+    }
+
+    const response = NextResponse.json({ success: true });
+
+    response.cookies.set('treepz_admin_token', json.data.token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+    });
+
+    return response;
+  } catch (error) {
+    console.error('Login error:', error);
     return NextResponse.json(
-      { message: json.message || "Invalid credentials" },
-      { status: 401 }
-    )
+      { message: 'Internal Server Error' },
+      { status: 500 },
+    );
   }
-
-  const response = NextResponse.json({ success: true })
-
-  response.cookies.set("treepz_admin_token", json.data.token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    path: "/",
-    maxAge: 60 * 60 * 24 * 7, 
-  })
-
-  return response
-} catch (error) {
-  console.error('Login error:', error)
-  return NextResponse.json({message: 'Internal Server Error'}, 
-    {status: 500}
-  )
-}
 }
